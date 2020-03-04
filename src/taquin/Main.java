57bd41f83a312f8acd7cdb2b7920127293d755ee
Lorada ANDRÉ <21316893+LoradaAndre@users.noreply.github.com>
2020-03-04 10:33:34 +0000

    feat: Ajout de la fenêtre de jeu

--- src/taquin/Main.java
+++ src/taquin/Main.java
@@ -1,9 +1,12 @@
 package taquin;
 
+import taquin.window.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-	
+
+		MainWindow window = new MainWindow(800,800,"Taquin game");
 	}
 
 }
--- src/taquin/window/MainWindow.java
+++ src/taquin/window/MainWindow.java
@@ -1,11 +1,20 @@
 package taquin.window;
 
+import javax.swing.JFrame;
+
 public class MainWindow {
-	private ImageTaquinGrid imageTaquinGrid;
-	private GUITaquinGrid guiTaquinGrid;
 
-	public MainWindow() {
+	// private ImageTaquinGrid imageTaquinGrid;
+	// private GUITaquinGrid guiTaquinGrid;
 
+	public MainWindow(int width, int height, String name) {
+
+		JFrame fenetre = new JFrame();
+		fenetre.setTitle(name);
+		fenetre.setSize(width, height);
+		fenetre.setLocationRelativeTo(null);
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenetre.setVisible(true);
 	}
 
 	/**
