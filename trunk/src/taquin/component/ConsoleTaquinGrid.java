8ac4d61c2f8bb2065bfc6f838a157f8dde9be2ef
21909920 <21909920@1986da17-93c0-4180-8a4d-6ccac8e895c2>
2020-03-04 09:50:05 +0000

    feat : Génération des fichiers avec Dia

---
+++ trunk/src/taquin/component/ConsoleTaquinGrid.java
@@ -0,0 +1,24 @@
+package taquin.component;
+
+public class ConsoleTaquinGrid implements TaquinGridObserver {
+	private TaquinGrid taquinGrid;
+
+	public ConsoleTaquinGrid() {
+
+	}
+
+	/**
+	 * @return void
+	 */
+	public void newGame() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @return Direction
+	 */
+	private Direction makeAMove() {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ trunk/src/taquin/component/ImageTaquinGrid.java
@@ -0,0 +1,10 @@
+package taquin.component;
+
+public class ImageTaquinGrid extends AbstractGUITaquinGrid {
+	private Image image;
+
+	public ImageTaquinGrid() {
+
+	}
+
+}
