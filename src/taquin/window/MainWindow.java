38d65485d99fa80cac409393978937a4df4e9d9f
Lorada ANDRÉ <21316893+LoradaAndre@users.noreply.github.com>
2020-03-04 11:18:41 +0000

    feat: Ajout de la barre de navigation de la fenêtre

--- src/taquin/window/MainWindow.java
+++ src/taquin/window/MainWindow.java
@@ -1,27 +1,54 @@
 package taquin.window;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
-public class MainWindow {
+public class MainWindow extends JFrame{
 
 	// private ImageTaquinGrid imageTaquinGrid;
 	// private GUITaquinGrid guiTaquinGrid;
 
-	public MainWindow(int width, int height, String name) {
+	public MainWindow(int w, int h, String name) {
 
-		JFrame fenetre = new JFrame();
-		fenetre.setTitle(name);
-		fenetre.setSize(width, height);
-		fenetre.setLocationRelativeTo(null);
-		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		fenetre.setVisible(true);
+		this.setTitle(name);
+		this.setSize(w, h);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		createMenuBar();
+		createMainUI();
+
+		//this.pack();
+		this.setVisible(true);
 	}
 
 	/**
 	 * @return void
 	 */
 	private void createMenuBar() {
-		// TODO Auto-generated method stub
+		JMenuBar menuBar = new JMenuBar();
+
+		JMenu menuFichier = new JMenu("Fichier");
+		JMenu menuAffichage = new JMenu("Mode");
+
+		JMenuItem itemNewGame = new JMenuItem("Nouvelle partie");
+		menuFichier.add(itemNewGame);
+
+		JMenuItem itemImage = new JMenuItem("Mode image");
+		menuAffichage.add(itemImage);
+
+		JMenuItem itemChiffre = new JMenuItem("Mode chiffres");
+		menuAffichage.add(itemChiffre);
+
+		menuBar.add(menuFichier);
+		menuBar.add(menuAffichage);
+
+		setJMenuBar(menuBar);
+	}
+
+	private void createMainUI() {
 	}
 
 }
