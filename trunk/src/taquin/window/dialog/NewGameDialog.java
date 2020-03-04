86f3c26325028ec45237b91cf42739bf5cb0e540
21909920 <21909920@1986da17-93c0-4180-8a4d-6ccac8e895c2>
2020-03-04 10:59:20 +0000

    feat : Classe mère pour les JDialog et JDialog pour commencer une nouvelle partie

--- trunk/src/taquin/window/dialog/NewGameDialog.java
+++ trunk/src/taquin/window/dialog/NewGameDialog.java
@@ -1,18 +1,78 @@
 package taquin.window.dialog;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
 public class NewGameDialog extends AbstractDialog {
+
+	private JTextField widthTextField;
+	private JTextField heightTextField;
+
 	private int width;
 	private int height;
 
-	public NewGameDialog() {
-
+	public NewGameDialog(JFrame parent) {
+		super(parent, "Nouvelle partie");
 	}
 
-	/**
-	 * @return JComponent
-	 */
+	@Override
 	protected JComponent getMainUI() {
-		// TODO Auto-generated method stub
+		JPanel mainContent = new JPanel();
+		mainContent.setLayout(new GridBagLayout());
+
+		GridBagConstraints c = new GridBagConstraints();
+
+		JLabel widthLabel = new JLabel("Largeur : ");
+		c.gridx = 0;
+		c.gridy = 0;
+		mainContent.add(widthLabel, c);
+
+		widthTextField = new JTextField();
+		widthTextField.setPreferredSize(new Dimension(200, 25));
+		c.gridx = 1;
+		c.gridy = 0;
+		mainContent.add(widthTextField, c);
+
+		JLabel heightLabel = new JLabel("Hauteur : ");
+		c.gridx = 0;
+		c.gridy = 1;
+		mainContent.add(heightLabel, c);
+
+		heightTextField = new JTextField();
+		heightTextField.setPreferredSize(new Dimension(200, 25));
+		c.gridx = 1;
+		c.gridy = 1;
+		mainContent.add(heightTextField, c);
+
+		return mainContent;
+	}
+
+	@Override
+	protected ActionListener getValidButtonEventHandler() {
+		return new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				NewGameDialog.this.dispose();
+			}
+		};
+	}
+
+	public Integer getSelectedWidth() {
+		try {
+			return Integer.parseInt(widthTextField.getText());
+		} catch(NumberFormatException e) {
+			return null;
+		}
+	}
+
+	public Integer getSelectedHeight() {
+		try {
+			return Integer.parseInt(heightTextField.getText());
+		} catch(NumberFormatException e) {
+			return null;
+		}
 	}
 
 }
