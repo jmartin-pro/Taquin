e3967a83c9f22dd613c1917d690e6a2cb8f522fd
21909920 <21909920@1986da17-93c0-4180-8a4d-6ccac8e895c2>
2020-03-04 14:48:39 +0000

    feat : Ajout de ConsoleTaquinGrid

--- trunk/src/taquin/Main.java
+++ trunk/src/taquin/Main.java
@@ -1,14 +1,19 @@
 package taquin;
 
 import javax.swing.*;
+
+import taquin.component.ConsoleTaquinGrid;
 import taquin.window.dialog.NewGameDialog;
 import taquin.window.*;
 
 public class Main {
 
 	public static void main(String[] args) {
+		ConsoleTaquinGrid consoleTaquinGrid = new ConsoleTaquinGrid(2, 2);
+		consoleTaquinGrid.newGame();
+
 		MainWindow window = new MainWindow(800,800,"Taquin game");
-		
+
 	}
 
 }
