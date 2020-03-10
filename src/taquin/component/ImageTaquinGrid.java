43c49a40455a72852e68d4997f5232b9946b29d8
Justine Martin <justine.martin.dev@gmail.com>
2020-03-10 09:55:46 +0000

    feat : Permet de dessiner une grille vide

--- src/taquin/component/ImageTaquinGrid.java
+++ src/taquin/component/ImageTaquinGrid.java
@@ -1,11 +1,29 @@
 package taquin.component;
 
+import java.awt.*;
+import java.awt.event.*;
+
+import taquin.core.TaquinGrid;
+
 @SuppressWarnings("serial")
 public class ImageTaquinGrid extends AbstractGUITaquinGrid {
-	//private Image image;
 
-	public ImageTaquinGrid() {
+	private Image image;
 
+	public ImageTaquinGrid(TaquinGrid taquinGrid) {
+		super(taquinGrid);
 	}
 
+	@Override
+	public void paintComponent(Graphics g) {
+		drawGrid(g);
+	}
+
+	public Image getImage() {
+		return this.image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
 }
