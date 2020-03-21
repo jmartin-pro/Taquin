11b23d6ca521174096597aacc42bae29ef008ed3
21909920 <21909920@1986da17-93c0-4180-8a4d-6ccac8e895c2>
2020-03-21 18:02:47 +0000

    chores : Ajoute de nouveau la structure pour les tests

---
+++ trunk/test/taquin/TaquinTest.java
@@ -0,0 +1,13 @@
+package taquin;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TaquinTest {
+
+	@Test
+	public void evaluatesExpression1() {
+		Assert.assertEquals(6, 6);
+	}
+
+}
