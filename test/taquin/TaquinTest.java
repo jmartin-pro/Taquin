8c4405fa34a3cafd982514634a03e087372d35cd
21806986 <55444884+Norah72@users.noreply.github.com>
2020-03-23 17:35:29 +0000

    feat : ajout de test unitaire pour la class TaquinGrid

--- test/taquin/TaquinTest.java
+++ test/taquin/TaquinTest.java
@@ -1,13 +1,43 @@
 package taquin;
 
+import taquin.core.TaquinGrid;
+import taquin.core.Direction;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TaquinTest {
 
 	@Test
-	public void evaluatesExpression1() {
-		Assert.assertEquals(6, 6);
+	public void melangeTrue() {
+		TaquinGrid gridCreate = new TaquinGrid(10, 10, false);
+		TaquinGrid gridRandomize = new TaquinGrid(10, 10);
+
+      	Assert.assertTrue(gridCreate != gridRandomize);
+	}
+
+	@Test
+	public void directionTrue() {
+		TaquinGrid gridCreate = new TaquinGrid(10, 10, false);
+		Assert.assertTrue(gridCreate.move(Direction.BAS));
+		Assert.assertTrue(gridCreate.move(Direction.DROITE));
+		Assert.assertTrue(gridCreate.move(Direction.HAUT));
+		Assert.assertTrue(gridCreate.move(Direction.GAUCHE));
+	}
+
+	@Test
+	public void finalyTrue() {
+		TaquinGrid gridCreate = new TaquinGrid(10, 10, false);
+		TaquinGrid gridRandomize = new TaquinGrid(10, 10);
+
+		Assert.assertTrue(gridCreate.finished());
+		Assert.assertFalse(gridRandomize.finished());
+	}
+
+	@Test
+	public void squareTrue() {
+		TaquinGrid gridCreate = new TaquinGrid(10, 10, false);
+		Assert.assertTrue(TaquinGrid.EMPTY_SQUARE == gridCreate.getSquare(9, 9));
 	}
 
 }
